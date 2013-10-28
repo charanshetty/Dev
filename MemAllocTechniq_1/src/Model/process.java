5bf2b0c9722321f4374b7219f0b3863b5c5f1618
charanshetty <charan.m.shetty@gmail.com>
2013-10-28 21:23:56 +0530

    with GUI changes

---
+++ MemAllocTechniq_1/src/Model/process.java
@@ -0,0 +1,20 @@
+package Model;
+
+public class process {
+int process_id;
+int mem_size;
+
+public int getProcess_id() {
+	return process_id;
+}
+public void setProcess_id(int process_id) {
+	this.process_id = process_id;
+}
+public int getMem_size() {
+	return mem_size;
+}
+public void setMem_size(int mem_size) {
+	this.mem_size = mem_size;
+}
+
+}
